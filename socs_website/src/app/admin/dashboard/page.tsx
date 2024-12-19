351704fd34074e16a905054e6cbd09b259be2773
D-Binara <dbkaveesha@gmail.com>
2024-12-19 23:46:21 +0530

    fix deployment error

--- socs_website/src/app/admin/dashboard/page.tsx
+++ socs_website/src/app/admin/dashboard/page.tsx
@@ -1,29 +1,10 @@
 'use client';
-import { useEffect, useState } from 'react';
+
 import { useRouter } from 'next/navigation';
-import { supabase } from '@/services/supabaseClient';
 
 export default function Dashboard() {
-    const [adminName, setAdminName] = useState<string | null>(null);
     const router = useRouter();
 
-    // Fetch admin details
-    // useEffect(() => {
-    //     const fetchAdminDetails = async () => {
-    //         const { data, error } = await supabase.auth.getUser();
-    //
-    //         if (error || !data.user) {
-    //             console.error('Error fetching admin details:', error?.message || 'No user found');
-    //             router.push('/public/login');
-    //             return;
-    //         }
-    //         // Extract admin name from user_metadata or fallback to email
-    //         const { user } = data;
-    //         setAdminName(user.user_metadata?.full_name || user.email || 'Admin');
-    //     };
-    //
-    //     fetchAdminDetails();
-    // }, [router]);
 
     // Logout handler
     const handleLogout = async () => {
@@ -43,7 +24,7 @@ export default function Dashboard() {
 
     return (
         <div>
-            <h1>Welcome, {adminName}</h1>
+            <h1>Welcome,</h1>
             <button onClick={handleLogout}>Logout</button>
         </div>
     );
